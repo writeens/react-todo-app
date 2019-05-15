1b335cd71cc9da848dd9ffc066a4617858be57fb
writeens <44687555+writeens@users.noreply.github.com>
2019-05-15 16:52:07 +0100

    Updates

---
+++ precache-manifest.27cac007bc384fefc3999f073553d08e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94b5fb4086d565031c59b0679cd9fb4d",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "eb745d1d0d56919fa3c5",
+    "url": "/react-todo-app/static/css/main.e40f9f0f.chunk.css"
+  },
+  {
+    "revision": "7be7b935a0c7a4fc9623",
+    "url": "/react-todo-app/static/js/2.698e5ffa.chunk.js"
+  },
+  {
+    "revision": "eb745d1d0d56919fa3c5",
+    "url": "/react-todo-app/static/js/main.58c2823c.chunk.js"
+  },
+  {
+    "revision": "b1aeed9f6344163e5e40",
+    "url": "/react-todo-app/static/js/runtime~main.a3a648b2.js"
+  }
+]);
